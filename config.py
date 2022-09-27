097658bfda166c56ae7e45926d598e822366faa9
guanfan27 <112605016+guanfan27@users.noreply.github.com>
2022-09-27 10:10:43 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employees.clfficy2iire.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employees"
+custombucket = "tohguanfan-bucket"
+customregion = "us-east-1"
